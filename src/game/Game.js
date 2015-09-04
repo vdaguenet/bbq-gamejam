b00750d3d9572e535252be3652962796fe613311
Valentin Daguenet <valentin.daguenet@gmail.com>
2015-09-04 19:03:28 +0200

    Add update and render methods

--- src/game/Game.js
+++ src/game/Game.js
@@ -1,11 +1,32 @@
 'use strict';
 
+import bindAll from 'lodash.bindAll';
+import raf from 'raf';
+
 export default class Game {
   constructor() {
+    bindAll(this, 'update');
+
     console.log('Game - construct');
   }
 
   start() {
     console.log('Game - start');
+
+    // TODO: init the game
+
+    this.update();
+  }
+
+  update() {
+    raf(this.update);
+
+    // TODO: update all the entities
+
+    this.render();
+  }
+
+  render() {
+    // TODO: render all the things
   }
 }
