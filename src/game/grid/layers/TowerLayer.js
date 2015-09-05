8b5f1ca1a97fb3173fbef8ecb28d4ad3f0e88482
Madjid Taha <contact@madjidtaha.fr>
2015-09-05 20:00:05 +0200

    Add TowerLayer

--- src/game/grid/layers/TowerLayer.js
+++ src/game/grid/layers/TowerLayer.js
@@ -1,25 +1,18 @@
 import PIXI from 'pixi.js';
-import Tile from '../Tile';
+import Tile from '../tile/Tile';
 import Loader from 'utils/Loader';
 
 export default class TowerLayer extends PIXI.Container {
   constructor(width, height, tileSize) {
     super();
-    console.log('towerLayer');
+
     this.tileSize = tileSize;
-    this.populate(width, height);
+    // this.populate(width, height);
   }
 
-  populate(width, height) {
-    const nbCols = Math.floor(width / this.tileSize);
-    const cols = Math.floor(nbCols / 3);
-    const nbRaws = Math.floor(height / this.tileSize);
-    const raws = Math.floor(nbRaws / 3);
+  addTower(x, y) {
 
-    for (let x = cols; x < nbCols - cols; x++) {
-      for (let y = raws; y < nbRaws - raws; y++) {
-        this.addChild(new Tile(x * this.tileSize, y * this.tileSize, Loader.getTexture('ennemy')));
-      }
-    }
+    // TODO Trouver la case correspondante au X, Y passé en paramètre et addChild la Tower correspondante
+
   }
 }
