98303e45983cef684febb1807ea7291064ca2109
Madjid Taha <contact@madjidtaha.fr>
2015-09-05 23:32:12 +0200

    Begin the Base

--- src/game/entities/Base.js
+++ src/game/entities/Base.js
@@ -1,11 +1,14 @@
+import PIXI from 'pixi.js';
+import Loader from 'utils/Loader';
 /**
  * Base class
  * Weaker tower
  */
-export default class Base {
+export default class Base extends PIXI.Sprite {
 
   constructor(options) {
 
+    super(Loader.getTexture('diver'));
     console.log('Base -> constructor');
 
     this.life = 20;
@@ -17,6 +20,13 @@ export default class Base {
 
   update() {
     console.warn('You should override update method on', Object.getPrototypeOf(this));
+  }
+
+  set life(value) {
+    if (value < 0) {
+      // TODO emit game over
+      console.log('Game Over');
+    }
   }
 
 }
